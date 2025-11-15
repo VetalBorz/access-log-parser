85150435c36c76e67f5acd3244d4d7b871877d74
Vitaly Borzunov <git.user.3039198@gmail.com>
2025-11-15 15:20:41 +0300

    Add file path validation with infinite loop and counter

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Случайное число от 0 до 1: " + Math.random());
+        Scanner scanner = new Scanner(System.in);
+        int correctFileCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                if (!fileExists) {
+                    System.out.println("Файл не существует");
+                }
+                if (isDirectory) {
+                    System.out.println("Указанный путь ведет к папке, а не к файлу");
+                }
+                continue;
+            }
+
+            correctFileCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + correctFileCount);
+        }
     }
 }
